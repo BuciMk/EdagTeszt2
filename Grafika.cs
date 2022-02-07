3bd294f700ae99ed800a0331b2f8482a8f1a6cb3
Buci <barsibenjamin@gmail.com>
2022-02-07 17:50:42 +0100

    feltoltes

---
+++ Grafika.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace EdagTeszt2
+{
+    internal class Grafika
+    {
+
+        private double forgatasiSzog = 0.0;
+        private double elozoForgatasiSzog = 0.0;
+        private Canvas canvas;
+        private Polygon haromszog;
+        private RotateTransform rotateTransform;
+
+        public Grafika(Canvas canvasBejovo)
+        {
+            canvas = canvasBejovo;
+        }
+
+        public void Rajz(Point pointA, Point pointB, Point pointC)
+        {
+            haromszog = new Polygon();
+            rotateTransform = new RotateTransform();
+            haromszog.Points.Add(pointA);
+            haromszog.Points.Add(pointB);
+            haromszog.Points.Add(pointC);
+            haromszog.Stroke = Brushes.Black;
+            canvas.Children.Add(haromszog);
+
+        }
+
+        public void haromszogForgatas(double bejovoForgatasiSzog)
+        {
+            if (haromszog == null)
+            {
+                return;
+            }
+
+            forgatasiSzog = elozoForgatasiSzog - bejovoForgatasiSzog;
+
+            var centerX = (haromszog.Points[0].X + haromszog.Points[1].X + haromszog.Points[2].X) / 3;
+            var centerY = (haromszog.Points[0].Y + haromszog.Points[1].Y + haromszog.Points[2].Y) / 3;
+
+            rotateTransform.Angle = forgatasiSzog;
+            rotateTransform.CenterX = centerX;
+            rotateTransform.CenterY = centerY;
+            haromszog.RenderTransform = rotateTransform;
+        }
+
+        public void Clear()
+        {
+            if (haromszog == null)
+            {
+                return;
+            }
+            canvas.Children.Clear();
+            haromszog.Points.Clear();
+            haromszog = null;
+            rotateTransform = null;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System.Windows;
+
+namespace EdagTeszt2
+{
+
+    public partial class MainWindow : Window
+    {
+        private Grafika grafikak;
+        private int koordinataEll = 2;
+        public MainWindow()
+        {
+            InitializeComponent();
+            grafikak = new Grafika(canvasPanel);
+        }
+
+        private void runButton_Click(object sender, RoutedEventArgs e)
+        {
+            scrollBar.Value = 0;
+            scbDoboz.Text = "0";
+            grafikak.Clear();
+
+            string[] aPontokStr = haromszogAOldal.Text.Split(',');
+            string[] bPontokStr = haromszogBOldal.Text.Split(',');
+            string[] cPontokStr = haromszogCOldal.Text.Split(',');
+            int[] aPontokInt = new int[aPontokStr.Length];
+            int[] bPontokInt = new int[bPontokStr.Length];
+            int[] cPontokInt = new int[cPontokStr.Length];
+
+            if (aPontokInt.Length != koordinataEll || bPontokInt.Length != koordinataEll || cPontokInt.Length != koordinataEll)
+            {
+                MessageBox.Show("Nem megfelelő koordináta formátum!");
+                return;
+            }
+
+            for (int i = 0; i < koordinataEll; i++)
+            {
+                int.TryParse(aPontokStr[i], out int numA);
+                aPontokInt[i] = numA;
+                int.TryParse(bPontokStr[i], out int numB);
+                bPontokInt[i] = numB;
+                int.TryParse(cPontokStr[i], out int numC);
+                cPontokInt[i] = numC;
+            }
+
+            var aPont = new Point(aPontokInt[0], aPontokInt[1]);
+            var bPont = new Point(bPontokInt[0], bPontokInt[1]);
+            var cPont = new Point(cPontokInt[0], cPontokInt[1]);
+
+            grafikak.Rajz(aPont, bPont, cPont);
+
+            scbDoboz.IsEnabled = true;
+            scrollBar.IsEnabled = true;
+            forgatasGomb.IsEnabled = true;
+        }
+
+        private void onScroll(object sender, System.Windows.Controls.Primitives.ScrollEventArgs e)
+        {
+            grafikak.haromszogForgatas(scrollBar.Value);
+            var s = $"{scrollBar.Value:0.00}";
+            scbDoboz.Text = s;
+
+        }
+
+        private void forgatasGomb_Click(object sender, RoutedEventArgs e)
+        {
+            double forgatasiSzog = 0.0;
+            double.TryParse(scbDoboz.Text, out forgatasiSzog);
+
+            if (forgatasiSzog < 0)
+            {
+                forgatasiSzog = 0;
+                scbDoboz.Text = forgatasiSzog.ToString();
+            }
+            else if (forgatasiSzog > 180)
+            {
+                forgatasiSzog = 180;
+                scbDoboz.Text = forgatasiSzog.ToString();
+            }
+
+            grafikak.haromszogForgatas(forgatasiSzog);
+            scrollBar.Value = forgatasiSzog;
+        }
+    }
+}
